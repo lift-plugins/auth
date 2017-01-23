e7dc4f6e75208e1ea736db9c2abaef64f45e3bb4
Camilo Aguilar <camilo.aguilar@gmail.com>
2017-01-23 16:34:03 -0500

    signout from original openidc provider

--- signout.go
+++ signout.go
@@ -15,7 +15,13 @@ import (
 func SignOut(address string) error {
 	defer tokens.Delete()
 
-	serverConn, err := grpc.Connection(address, "lift-auth")
+	tks := new(tokens.Tokens)
+	if err := tks.Read(); err != nil {
+		ui.Debug("%+v", errors.Wrap(err, "we were unable to revoke tokens in the server"))
+		return nil
+	}
+
+	serverConn, err := grpc.Connection(tks.Issuer, "lift-auth")
 	if err != nil {
 		// We were unable to revoke tokens in the server, so we just return
 		// and let them expire.
@@ -27,7 +33,11 @@ func SignOut(address string) error {
 	client := api.NewAuthzClient(serverConn)
 	ctx := context.Background()
 
-	if _, err := client.SignOut(ctx, new(api.SignOutRequest)); err != nil {
+	if _, err := client.SignOut(ctx, &api.SignOutRequest{
+		ClientId:     "",
+		ClientSecret: "",
+		IdToken:      tks.ID,
+	}); err != nil {
 		ui.Debug("%+v", errors.Wrap(err, "failed signing user out from identity server"))
 	}
 	return nil
